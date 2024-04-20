121ac770a97a8efa149179552ac8b7f47c852014
[Judith Aragones Sirera] <[jaragonessi@uoc.edu]>
2024-04-20 17:28:05 +0200

    20240420 v0.2 footer einesHTMLCSS Judith Aragones

---
+++ dist/concerts.90cbb604.css
@@ -0,0 +1,76 @@
+body {
+  color: #fff;
+  text-align: center;
+  background-image: url("backgroundConcerts.f0577bc9.jpg");
+  background-size: cover;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, sans-serif;
+  font-size: 15pt;
+  overflow-y: scroll;
+}
+
+header {
+  color: #fff;
+  background-color: #333;
+  padding: 10px 20px;
+}
+
+header nav ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+header nav ul li {
+  margin-right: 20px;
+  display: inline;
+}
+
+header nav ul li a {
+  color: #fff;
+  text-decoration: none;
+}
+
+.content {
+  padding: 20px;
+}
+
+.background {
+  background-image: url("background_blaumut.008bfb42.jpeg");
+  background-size: cover;
+  background-attachment: fixed;
+  height: 100vh;
+}
+
+.footer-content {
+  color: #fff;
+  text-align: center;
+  background-color: #000;
+  width: 100%;
+  padding: 5px 0;
+  position: fixed;
+  bottom: 0;
+}
+
+table {
+  border-collapse: collapse;
+  width: 70%;
+}
+
+th {
+  text-align: center;
+  background-color: #d2691e;
+  border: 1px solid #fff;
+  padding: 8px;
+}
+
+td {
+  text-align: left;
+  border: 1px solid #000;
+  padding: 8px;
+}
+/*# sourceMappingURL=concerts.90cbb604.css.map */
